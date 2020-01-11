b0ecf49b0f8504454ea98d4941c9e5548f8c07a9
Sander Rõuk <sander@rouk.io>
2020-01-11 22:03:28 +0200

    Add edge pinning and dimension matching

--- LayoutDemo/LayoutDemo/Extensions/UIView+Pin.swift
+++ LayoutDemo/LayoutDemo/Extensions/UIView+Pin.swift
@@ -126,17 +126,14 @@ public extension UIView {
     ///
     /// - Parameters:
     ///     - Dimension: The dimension which will be set to the specified size.
-    ///
     ///     - size: The specific size which the dimension will be set to.
-    ///
     ///     - relation: The relation between the first attribute and the modified second attribute
     ///      in a constraint.
-    ///
-    ///      - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
     ///      constraints are more important, allowing the system to make appropriate tradeoffs when
     ///      satisfying the constraints of the system as a whole.
-    ///
-    ///      - multiplier: The multiplier which is used to modify the size. A size 100 with a multiplier of 0.5
+    ///     - multiplier: The multiplier which is used to modify the size. A size 100 with a multiplier
+    ///     of 0.5
     ///      would result in the dimension size of 50.
     ///
     /// - Returns: `NSLayoutConstraint` matching the appropriate anchor for the size constraint.
@@ -164,10 +161,309 @@ public extension UIView {
         return constraint
     }
 
+    /// Sets the specified dimensions of the view to match the same dimensions of the view's superview.
+    ///
+    /// - Returns: An array of `NSLayoutConstraint` matching the created constraint.
+    ///     The constraints will be in the following order: width, height
+    ///
     @discardableResult
+    func matchDimensionsToSuperview() -> [NSLayoutConstraint] {
+        let superview = unwrapSuperviewOrFailure()
+        return matchDimensionsTo(view: superview)
+    }
+
+    /// Sets the specified dimensions of the view to match the same dimensions of the target view.
+    ///
+    /// - Parameter view: The view which the dimensions will be constraint to.
+    ///
+    /// - Returns: An array of `NSLayoutConstraint` matching the created constraint.
+    ///     The constraints will be in the following order: width, height
+    ///
+    @discardableResult
+    func matchDimensionsTo(view: UIView) -> [NSLayoutConstraint] {
+        return [
+            matchDimension(.width, to: view),
+            matchDimension(.height, to: view)
+        ]
+    }
+
+    /// Sets the specified dimension of the view to match the same dimension of the target view.
+    ///
+    /// - Parameters:
+    ///     - dimension: The dimension which will be matched to the target view's same dimension.
+    ///     - view: Target view which will be constrained to.
+    ///     - relation: The relation between the first attribute and the modified second attribute
+    ///      in a constraint.
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///      constraints are more important, allowing the system to make appropriate tradeoffs when
+    ///      satisfying the constraints of the system as a whole.
+    ///     - constant: A constant added to the new dimension constraint.
+    ///     - multiplier: Size difference multiplier.
+    ///
+    /// - Returns: `NSLayoutConstraint` matching the created constraint.
+    ///
+    @discardableResult
+    func matchDimension(
+        _ dimension: LayoutDimension,
+        to view: UIView,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat = 0,
+        multiplier: CGFloat = 1
+    ) -> NSLayoutConstraint {
+        return matchDimension(
+            dimension,
+            to: dimension,
+            of: view,
+            relation: relation,
+            priority: priority,
+            constant: constant,
+            multiplier: multiplier
+        )
+    }
+
+    /// Sets the specified dimension of the view to match the specified dimension of the target view.
+    ///
+    /// - Parameters:
+    ///     - dimension: The dimension which will be matched to the target view's target dimension.
+    ///     - viewDimension: Target view's dimension which will be constrained to.
+    ///     - view: Target view which will be constrained to.
+    ///     - relation: The relation between the first attribute and the modified second attribute
+    ///      in a constraint.
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///      constraints are more important, allowing the system to make appropriate tradeoffs when
+    ///      satisfying the constraints of the system as a whole.
+    ///     - constant: A constant added to the new dimension constraint.
+    ///     - multiplier: Size difference multiplier.
+    ///
+    /// - Returns: `NSLayoutConstraint` matching the created constraint.
+    ///
+    @discardableResult
+    func matchDimension(
+        _ dimension: LayoutDimension,
+        to viewDimension: LayoutDimension,
+        of view: UIView,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat = 0,
+        multiplier: CGFloat = 1
+    ) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = NSLayoutConstraint(
+            item: self,
+            attribute: dimension == .height ? .height : .width,
+            relatedBy: relation,
+            toItem: view,
+            attribute: dimension == .height ? .height : .width,
+            multiplier: multiplier,
+            constant: constant
+        )
+
+        constraint.priority = priority
+        constraint.isActive = true
+
+        return constraint
+    }
+
+    /// Sets the view's edges equal to the same edges of the view's superview.
+    ///
+    /// - Parameters:
+    ///     - edges: The specific edges of the view which will be constrained, defaults to All edges.
+    ///     - insets: The edge insets adding spacing between the edge insets. An edge set of [.top, .bottom]
+    ///     with an inset set of .top(10) + .bottom(10) would result in constraints where the top edge
+    ///     of the view being constraint is 10 units below the target view and the bottom is above the target
+    ///     view by 10 units.
+    ///
+    /// - Returns: An array of  `NSLayoutConstraint` matching the appropriate layout constraints in
+    ///  the following order: Top, Leading, Trailing, Bottom.
+    ///
+    @discardableResult
+    func pinEdgesToSuperviewEdges(
+        _ edges: [LayoutEdge] = .all,
+        with insets: UIEdgeInsets = .zero
+    ) -> [NSLayoutConstraint] {
+        let superview = unwrapSuperviewOrFailure()
+        return pinEdges(edges, to: superview, insets: insets)
+    }
+
+    /// Sets the view's edge equal to the same edge of the view's superview.
+    ///
+    /// - Parameters:
+    ///     - edge: The specific edge of the view which will be constrained.
+    ///     - relation: The relation between the first attribute and the modified second attribute
+    ///      in a constraint.
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///      constraints are more important, allowing the system to make appropriate tradeoffs when
+    ///      satisfying the constraints of the system as a whole.
+    ///     - constant: The constant which will be added to the resulting constraint. Adding a constant of
+    ///     50 would result in the resulting constraint to be offset by 50 units on the appropriate axis.
+    ///     - multiplier: The multiplier which is used to modify the size. A size 100 with a multiplier
+    ///     of 0.5
+    ///      would result in the dimension size of 50.
+    ///
+    /// - Returns: `NSLayoutConstraint` matching the appropriate layout constraint generated.
+    ///
+    @discardableResult
+    func pinEdgeToSuperview(
+        edge: LayoutEdge,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat = 0,
+        multiplier: CGFloat = 1
+    ) -> NSLayoutConstraint {
+        let superview = unwrapSuperviewOrFailure()
+        return pinEdge(
+            edge,
+            to: edge,
+            of: superview,
+            relation: relation,
+            priority: priority,
+            constant: constant,
+            multiplier: multiplier
+        )
+    }
+
+    /// Sets the view's edges equal to the same edges of another view.
+    ///
+    /// - Parameters:
+    ///     - edges: The specific edges of the view which will be constrained.
+    ///     - view: Target view which will be constrained to.
+    ///     - insets: The edge insets adding spacing between the edge insets. An edge set of [.top, .bottom]
+    ///     with an inset set of .top(10) + .bottom(10) would result in constraints where the top edge
+    ///     of the view being constraint is 10 units below the target view and the bottom is above the target
+    ///     view by 10 units.
+    ///
+    /// - Returns: An array of  `NSLayoutConstraint` matching the appropriate layout constraints in
+    ///  the same order as the param `edges` was provided generated.
+    ///
+    @discardableResult
+    func pinEdges(
+        _ edges: [LayoutEdge],
+        to view: UIView,
+        insets: UIEdgeInsets = .zero
+    ) -> [NSLayoutConstraint] {
+        return edges.map({ edge in
+            pinEdge(
+                edge,
+                to: edge,
+                of: view,
+                constant: translateToConstantFrom(inset: insets, for: edge)
+            )
+        })
+    }
+
+    /// Sets the view's edge equal to the same edge of another view.
+    ///
+    /// - Parameters:
+    ///     - edge: The specific edge of the view which will be constrained.
+    ///     - view: Target view which will be constrained to.
+    ///     - relation: The relation between the first attribute and the modified second attribute
+    ///      in a constraint.
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///      constraints are more important, allowing the system to make appropriate tradeoffs when
+    ///      satisfying the constraints of the system as a whole.
+    ///     - constant: The constant which will be added to the resulting constraint. Adding a constant of
+    ///     50 would result in the resulting constraint to be offset by 50 units on the appropriate axis.
+    ///     - multiplier: The multiplier which is used to modify the size. A size 100 with a multiplier
+    ///     of 0.5
+    ///      would result in the dimension size of 50.
+    ///
+    /// - Returns: `NSLayoutConstraint` matching the appropriate layout constraint generated.
+    ///
+    @discardableResult
+    func pinEdge(
+        _ edge: LayoutEdge,
+        to view: UIView,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat = 0,
+        multiplier: CGFloat = 1
+    ) -> NSLayoutConstraint {
+        return pinEdge(
+            edge,
+            to: edge,
+            of: view,
+            relation: relation,
+            priority: priority,
+            constant: constant,
+            multiplier: multiplier
+        )
+    }
+
+    /// Sets the view's edge equal to the specified edge of another view.
+    ///
+    /// - Parameters:
+    ///     - edge: The specific edge of the view which will be constrained.
+    ///     - viewEdge: The target view's edge which will be constrained to.
+    ///     - view: Target view which will be constrained to.
+    ///     - relation: The relation between the first attribute and the modified second attribute
+    ///      in a constraint.
+    ///     - priority: The layout priority is used to indicate to the constraint-based layout system which
+    ///      constraints are more important, allowing the system to make appropriate tradeoffs when
+    ///      satisfying the constraints of the system as a whole.
+    ///     - constant: The constant which will be added to the resulting constraint. Adding a constant of
+    ///     50 would result in the resulting constraint to be offset by 50 units on the appropriate axis.
+    ///     - multiplier: The multiplier which is used to modify the size. A size 100 with a multiplier
+    ///     of 0.5
+    ///      would result in the dimension size of 50.
+    ///
+    /// - Returns: `NSLayoutConstraint` matching the appropriate layout constraint generated.
+    ///
+    @discardableResult
+    func pinEdge(
+        _ edge: LayoutEdge,
+        to viewEdge: LayoutEdge,
+        of view: UIView,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat = 0,
+        multiplier: CGFloat = 1
+    ) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = NSLayoutConstraint(
+            item: self,
+            attribute: translateToAttributeFrom(edge: edge),
+            relatedBy: relation,
+            toItem: view,
+            attribute: translateToAttributeFrom(edge: viewEdge),
+            multiplier: multiplier,
+            constant: constant
+        )
+
+        constraint.priority = priority
+        constraint.isActive = true
+        return constraint
+    }
+
     private func unwrapSuperviewOrFailure() -> UIView {
         guard let superview = superview else { preconditionFailure("The view's superview can't be nil") }
         return superview
+    }
+
+    private func translateToAttributeFrom(edge: LayoutEdge) -> NSLayoutConstraint.Attribute {
+        switch edge {
+        case .top: return .top
+        case .left: return .left
+        case .leading: return .leading
+        case .right: return .right
+        case .trailing: return .trailing
+        case .bottom: return .bottom
+        }
+    }
+
+    private func translateToConstantFrom(inset: UIEdgeInsets, for edge: LayoutEdge) -> CGFloat {
+
+        let isLeftToRight = UIView.userInterfaceLayoutDirection(for: semanticContentAttribute) == .leftToRight
+        switch edge {
+        case .top: return inset.top
+        case .left: return inset.left
+        case .leading: return isLeftToRight ? inset.left : -inset.right
+        case .right: return -inset.right
+        case .trailing: return isLeftToRight ? -inset.right : inset.left
+        case .bottom: return -inset.bottom
+        }
     }
 }
 
@@ -189,4 +485,36 @@ public enum LayoutDimension {
 
     /// Corresponds to `NSLayoutConstraint.Attribute.width`
     case width
+}
+
+public enum LayoutEdge {
+    case top
+    case left
+    case leading
+    case right
+    case trailing
+    case bottom
+}
+
+// Mark: Extensions
+public extension Collection where Element == LayoutEdge {
+    static var all: [LayoutEdge] {
+        return [.top, .leading, .trailing, .bottom]
+    }
+
+    static func all(except edge: LayoutEdge) -> [LayoutEdge] {
+        var edges: [LayoutEdge] = .all
+        switch edge {
+        case .top:
+            edges.removeAll(where: { $0 == .top })
+        case .left, .leading:
+            edges.removeAll(where: { $0 == .leading })
+        case .bottom:
+            edges.removeAll(where: { $0 == .bottom })
+        case .right, .trailing:
+            edges.removeAll(where: { $0 == .trailing })
+        }
+
+        return edges
+    }
 }
--- LayoutDemo/LayoutDemo/ViewController.swift
+++ LayoutDemo/LayoutDemo/ViewController.swift
@@ -4,6 +4,11 @@ class ViewController: UIViewController {
 
     private let redCircle = UIView()
     private let whiteBlob = UIView()
+    private let orangeSquare = UIView()
+    private let greenSquare = UIView()
+    private let yellowSquare = UIView()
+    private let tealQuadlateral = UIView()
+    private let pinkBlob = UIView()
 
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -24,17 +29,56 @@ class ViewController: UIViewController {
 
         whiteBlob.backgroundColor = .white
         whiteBlob.layer.cornerRadius = 5
+
+        orangeSquare.backgroundColor = .orange
+        greenSquare.backgroundColor = .green
+        yellowSquare.backgroundColor = .yellow
+        tealQuadlateral.backgroundColor = .systemTeal
+
+        pinkBlob.backgroundColor = .systemPink
+        pinkBlob.layer.cornerRadius = 25
     }
 
     private func layout() {
+        // Red circle centered in the view (r=25)
         view.addSubview(redCircle)
         redCircle.centerInSuperview()
         redCircle.setDimensions(to: .equalSides(of: 50))
 
+        // White blob centered in the red square with dimensions of 30x20
         view.addSubview(whiteBlob)
         whiteBlob.centerIn(view: redCircle)
         whiteBlob.setDimension(.height, toSize: 20)
         whiteBlob.setDimension(.width, toSize: 30)
+
+        // Orange square 50x50 aligned to horizontal axis, pinned to superview right
+        // 20 units away from right edge
+        view.addSubview(orangeSquare)
+        orangeSquare.alignAxisToSuperview(axis: .horizontal)
+        orangeSquare.pinEdgeToSuperview(edge: .right, constant: -20)
+        orangeSquare.setDimensions(to: .equalSides(of: 50))
+
+        // Green square pinned to orange squares edges with insets of 20
+        orangeSquare.addSubview(greenSquare)
+        greenSquare.pinEdgesToSuperviewEdges(with: .uniform(20))
+
+        // Yellow square matching green squares dimensions aligned to orange square's
+        // vertical axis and placed 20 units above orange square
+        view.addSubview(yellowSquare)
+        yellowSquare.matchDimensionsTo(view: greenSquare)
+        yellowSquare.alignAxisTo(axis: .vertical, ofView: orangeSquare)
+        yellowSquare.pinEdge(.bottom, to: .top, of: orangeSquare, constant: -20)
+
+        // Teal quadlateral aligned to superview's horizontal axis
+        // Pinned to superview's all edges except right, with insets of 20
+        // width set to 50
+        view.addSubview(tealQuadlateral)
+        tealQuadlateral.pinEdgesToSuperviewEdges(.all(except: .right), with: .uniform(20))
+        tealQuadlateral.setDimension(.width, toSize: 50)
+
+        tealQuadlateral.addSubview(pinkBlob)
+        pinkBlob.matchDimensionsToSuperview()
+        pinkBlob.centerInSuperview()
     }
 
 }
@@ -42,6 +86,45 @@ class ViewController: UIViewController {
 private extension CGSize {
     static func equalSides(of size: CGFloat) -> CGSize {
         return .init(width: size, height: size)
+    }
+}
+
+private extension UIEdgeInsets {
+    static func top(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: size, left: 0, bottom: 0, right: 0)
+    }
+
+    static func bottom(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: size, right: 0)
+    }
+
+    static func left(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: size, left: 0, bottom: size, right: 0)
+    }
+
+    static func right(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: size)
+    }
+
+    static func horizontal(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: size, bottom: 0, right: size)
+    }
+
+    static func vertical(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: size, left: 0, bottom: size, right: 0)
+    }
+
+    static func uniform(_ size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: size, left: size, bottom: size, right: size)
+    }
+
+    static func + (lhs: UIEdgeInsets, rhs: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: lhs.top + rhs.top,
+            left: lhs.left + rhs.left,
+            bottom: lhs.bottom + rhs.bottom,
+            right: lhs.right + rhs.right
+        )
     }
 }
 
