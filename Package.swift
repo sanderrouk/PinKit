8b39b808c644f0fda95fda5850a8233a8a3eb86e
Sander Rõuk <sander@rouk.io>
2020-02-05 22:00:48 +0200

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PinKit",
+    platforms: [.iOS(.v10)], products: [.library(name: "PinKit",targets: ["PinKit"])],
+    dependencies: [],
+    targets: [.target(name: "PinKit", dependencies: [])]
+)
