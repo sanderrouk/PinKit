a53cbf5d988ef27be8568997f17e08a5133f35d1
Sander Rõuk <sander@rouk.io>
2020-02-05 23:46:11 +0200

    Add Carthage support

---
+++ PinKit/PinKit.h
@@ -0,0 +1,18 @@
+//
+//  PinKit.h
+//  PinKit
+//
+//  Created by Sander Rõuk on 05/02/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinKit.
+FOUNDATION_EXPORT double PinKitVersionNumber;
+
+//! Project version string for PinKit.
+FOUNDATION_EXPORT const unsigned char PinKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinKit/PublicHeader.h>
+
+
